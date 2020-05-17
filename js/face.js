83b0db59faadcefa2d41940c51a606144a72752d
thespite <the.spite@gmail.com>
2020-05-17 14:42:08 +0100

    Added support for video texture mapping.

--- js/face.js
+++ js/face.js
@@ -1,15 +1,22 @@
-import { FACES as indices, UVS as texCoords } from './geometry.js';
-import { BufferGeometry, BufferAttribute, Vector3, Triangle, Matrix4 } from '../third_party/three.module.js';
+import { FACES as indices, UVS as texCoords } from "./geometry.js";
+import {
+  BufferGeometry,
+  BufferAttribute,
+  Vector3,
+  Triangle,
+  Matrix4,
+} from "../third_party/three.module.js";
 
 class FaceMeshFaceGeometry extends BufferGeometry {
-  constructor() {
+  constructor(options = {}) {
     super();
 
+    this.useVideoTexture = options.useVideoTexture || false;
     this.flipped = false;
     this.positions = new Float32Array(468 * 3);
     this.uvs = new Float32Array(468 * 2);
-    this.setAttribute('position', new BufferAttribute(this.positions, 3));
-    this.setAttribute('uv', new BufferAttribute(this.uvs, 2));
+    this.setAttribute("position", new BufferAttribute(this.positions, 3));
+    this.setAttribute("uv", new BufferAttribute(this.uvs, 2));
     this.setUvs();
     this.setIndex(indices);
     this.computeVertexNormals();
@@ -22,10 +29,24 @@ class FaceMeshFaceGeometry extends BufferGeometry {
 
   setUvs() {
     for (let j = 0; j < 468 * 2; j += 2) {
-      this.uvs[j] = this.flipped ? 1 - (texCoords[j] / 4096) : (texCoords[j] / 4096);
-      this.uvs[j + 1] = 1 - (texCoords[j + 1]) / 4096;
+      this.uvs[j] = this.flipped
+        ? 1 - texCoords[j] / 4096
+        : texCoords[j] / 4096;
+      this.uvs[j + 1] = 1 - texCoords[j + 1] / 4096;
     }
-    this.getAttribute('uv').needsUpdate = true;
+    this.getAttribute("uv").needsUpdate = true;
+  }
+
+  setVideoUvs() {
+    let ptr = 0;
+    for (let j = 0; j < 468 * 2; j += 2) {
+      this.uvs[j] = this.flipped
+        ? this.positions[ptr] / this.w + 0.5
+        : 1 - (this.positions[ptr] / this.w + 0.5);
+      this.uvs[j + 1] = this.positions[ptr + 1] / this.h + 0.5;
+      ptr += 3;
+    }
+    this.getAttribute("uv").needsUpdate = true;
   }
 
   setSize(w, h) {
@@ -34,23 +55,27 @@ class FaceMeshFaceGeometry extends BufferGeometry {
   }
 
   update(face, cameraFlipped) {
-    if (cameraFlipped !== this.flipped) {
-      this.flipped = cameraFlipped;
-      this.setUvs();
-    }
     let ptr = 0;
     for (const p of face.scaledMesh) {
       if (cameraFlipped) {
-        this.positions[ptr] = p[0] + .5 * this.w;
+        this.positions[ptr] = p[0] + 0.5 * this.w;
       } else {
-        this.positions[ptr] = p[0] - .5 * this.w;
+        this.positions[ptr] = p[0] - 0.5 * this.w;
       }
-      this.positions[ptr + 1] = (this.h - p[1]) - .5 * this.h;
+      this.positions[ptr + 1] = this.h - p[1] - 0.5 * this.h;
       this.positions[ptr + 2] = -p[2];
       ptr += 3;
     }
     this.attributes.position.needsUpdate = true;
     this.computeVertexNormals();
+    if (this.useVideoTexture) {
+      this.setVideoUvs();
+    } else {
+      if (cameraFlipped !== this.flipped) {
+        this.flipped = cameraFlipped;
+        this.setUvs();
+      }
+    }
   }
 
   track(id0, id1, id2) {
@@ -74,4 +99,4 @@ class FaceMeshFaceGeometry extends BufferGeometry {
   }
 }
 
-export { FaceMeshFaceGeometry }
+export { FaceMeshFaceGeometry };
